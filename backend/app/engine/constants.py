df72a4b5e8ff06a925b47c869279bf90dea6d665
Ramolis Systems LLC <ramolisdenneyous@gmail.com>
2026-06-16 15:51:45 -0700

    Publish backend/app/engine/constants.py

---
+++ backend/app/engine/constants.py
@@ -0,0 +1,8 @@
+OPPOSITION_AGENT_SLOT = 12
+OPPOSITION_INITIATIVE_ID = "opp:12"
+OPPOSITION_DISPLAY_NAME = "Opposition"
+OPPOSITION_CLEANUP_DELAY_SECONDS = 5
+
+SLOT_COLORS = {1: "red", 2: "orange", 3: "yellow", 4: "green"}
+MONSTER_INSTANCE_LABELS = ["Monster-One", "Monster-Two", "Monster-Three", "Monster-Four"]
+
