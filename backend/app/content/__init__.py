8c0e9dcdb5b5ea8499483521f0fd2740000c2503
Ramolis Systems LLC <ramolisdenneyous@gmail.com>
2026-06-16 15:51:38 -0700

    Publish backend/app/content/__init__.py

---
+++ backend/app/content/__init__.py
@@ -0,0 +1,5 @@
+"""Content packs and registries for Story Engine games."""
+
+from .registry import ACTIVE_CONTENT_PACK_ID, CONTENT_PACKS, ContentPack, get_content_pack, validate_content_pack
+
+__all__ = ["ACTIVE_CONTENT_PACK_ID", "CONTENT_PACKS", "ContentPack", "get_content_pack", "validate_content_pack"]
